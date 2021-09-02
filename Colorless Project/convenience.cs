d3c5468d8abdc3e86bcca3466b04669d9dfa8059
vpdh98 <33365675+vpdh98@users.noreply.github.com>
2021-09-02 23:06:07 +0900

    Update convenience.cs

--- Colorless Project/convenience.cs
+++ Colorless Project/convenience.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Reflection;
+using System.Collections.Generic;
+using System.Linq;
 
 public static class Convenience{
 	private static int logNum = 1;
@@ -11,5 +13,29 @@ public static class Convenience{
 	public static void testLog(String s){
 		Console.WriteLine(logNum+":"+s);
 		logNum++;
+	}
+	
+	public static void testLog(int n){
+		Console.WriteLine(logNum+":"+n);
+		logNum++;
+	}
+	
+	public static void testLog(bool b){
+		Console.WriteLine(logNum+":"+b);
+		logNum++;
+	}
+	
+	public static bool isEmptyList<T>(List<T> list){
+		if(list == null){
+			Console.WriteLine("이 리스트는 null 입니다.");
+			return false;
+		}
+		if(list.Any()){
+			Console.WriteLine("이 리스트는 비었습니다.");
+			return false;
+		}else{
+			Console.WriteLine("이 리스트는 비지 않았습니다.");
+			return true;
+		}
 	}
 }
