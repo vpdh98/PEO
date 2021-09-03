f9565eed63a43fbc52cc389f418e4bb20eda6145
vpdh98 <33365675+vpdh98@users.noreply.github.com>
2021-09-03 23:41:28 +0900

    Update choice.cs
    모호한 필드 이름 변경, 불필요한 메서드,생성자 제거

--- Colorless Project/choice.cs
+++ Colorless Project/choice.cs
@@ -6,7 +6,7 @@ using static Convenience;
 using System.Linq;
 
 public enum ChoiceType{
-			CHOICE,		//선택지
+			NORMAL,		//선택지
 			BATTLE,		//전투
 			TALK,		//대화
 			GET,		//획득
@@ -17,264 +17,214 @@ public enum ChoiceType{
 
 public class Choice : ICloneable //선택지 부여 하는 클래스
 {
-		public int CTNum;      //ChoiceTextNumber
-		public int OSTNum;		//OnlyShowTextNumber
-		public int STNum;
-		public int BGTNum;
-		public String Name {get;set;}
-		public delegate void Quick();
-		public Quick QuickDelegate{get;set;} 
+	public int selectTextNum;      
+	public int onlyShowTextNum;		
+	public int streamTextNum;
+	public int backgroundTextNum;
+	public String Name {get;set;}
+	public delegate void Quick();
+	public Quick QuickDelegate{get;set;} 
+
+	private List<TextAndPosition> selectText;
+	private List<TextAndPosition> onlyShowText;
+	private List<TextAndPosition> streamText;
+	private List<TextAndPosition> backgroundText;
+	private List<TextAndPosition> returnText;
+	private Dictionary<int,String> indicateChoice;
+	private List<Monster> monsterList;
+	private List<NPC> npcList;
+	private ChoiceType choiceType;
 		
-		
-		
-		public Choice(){
-			CTNum = 0;
-			OSTNum = 0;
-			STNum = 0;
-			BGTNum = 0;
-			indicateChoice = new Dictionary<int,String>();
-			backgroundText = new List<TextAndPosition>();
-			choiceText = new List<TextAndPosition>();
-			onlyShowText = new List<TextAndPosition>();
-			streamText = new List<TextAndPosition>();
-			choiceType = ChoiceType.CHOICE;
+	public Choice(){
+		selectTextNum = 0;
+		onlyShowTextNum = 0;
+		streamTextNum = 0;
+		backgroundTextNum = 0;
+		selectText = new List<TextAndPosition>();
+		onlyShowText = new List<TextAndPosition>();
+		streamText = new List<TextAndPosition>();
+		backgroundText = new List<TextAndPosition>();
+		indicateChoice = new Dictionary<int,String>();
+		monsterList = new List<Monster>();
+		npcList = new List<NPC>();
+		choiceType = ChoiceType.NORMAL;
+	}
+
+
+	public List<TextAndPosition> SelectText{
+		get{
+			return selectText;
 		}
-		
-		List<TextAndPosition> choiceText;
-		public List<TextAndPosition> ChoiceText{
-			get{
-				return choiceText;
+
+		set{
+			selectText = value;
+			selectTextNum = SelectText.Count;
+		}
+	}
+	public List<TextAndPosition> OnlyShowText{
+		get{
+			return onlyShowText;
+		}
+		set{
+			onlyShowText = value;
+			onlyShowTextNum = OnlyShowText.Count;
+		}
+	}
+	public List<TextAndPosition> StreamText{
+		get{
+			return streamText;
+		}
+		set{
+			streamText = value;
+			streamTextNum = streamText.Count;
+			foreach(TextAndPosition text in streamText){
+					text.isStream = true;
 			}
 			
-			set{
-				choiceText = value;
-				SetCTNum();
+		}
+	}	
+	public List<TextAndPosition> BackgroundText{
+		get{
+			return backgroundText;
+		}
+		set{
+			backgroundText = value;
+			backgroundTextNum = backgroundText.Count;
+		}
+	}
+	public List<TextAndPosition> ReturnText{
+		get{
+			return returnText;
+		}
+		set{
+			returnText = value;
+		}
+	}
+	public Dictionary<int,String> IndicateChoice{
+		get{
+			return indicateChoice;
+		}
+		set{
+			indicateChoice = value;
+		}
+	}
+	public List<Monster> MonsterList{
+		get{
+			return monsterList;
+		}
+		set{
+			if(isEmptyList(value))
+				testLog("몬스터 리스트 초기화");
+			monsterList = value;
+		}
+	}
+	public List<NPC> NPCList{
+		get{
+			return npcList;
+		}
+		set{
+			npcList = value;
+		}
+	}
+	public ChoiceType ChoiceType{
+		get;
+		set;
+	}
+	public String GetChoiceOn(int num){ //다음 선택지를 indicateChoice에서 찾아 반환
+		return indicateChoice[num];
+	}
+
+	public void QuickRun(){
+		QuickDelegate();
+	}
+
+	public String QuickNext(){  //choiceType이 QUICKNEXT일때 빠르게 다음 선택지로 넘어갈때 DTG또는 Main에서 호출하는 함수
+		//Console.ReadKey();
+		return GetChoiceOn(0);
+	}
+
+	public void LeaveChoice(){
+		if(ReturnText != null){
+			OnlyShowText = ReturnText;
+		}
+	}
+
+	protected Choice(Choice that):this(){
+		
+		if(that.SelectText != null)
+			this.SelectText = that.SelectText.ConvertAll(new Converter<TextAndPosition, TextAndPosition>(o => (TextAndPosition)o.Clone()));
+		if(that.OnlyShowText != null)
+			this.OnlyShowText = that.OnlyShowText.ConvertAll(new Converter<TextAndPosition, TextAndPosition>(o => (TextAndPosition)o.Clone()));
+		if(that.StreamText != null)
+			this.StreamText = that.StreamText.ConvertAll(new Converter<TextAndPosition, TextAndPosition>(o => (TextAndPosition)o.Clone()));
+		if(that.BackgroundText != null)
+			this.BackgroundText = that.BackgroundText.ConvertAll(new Converter<TextAndPosition, TextAndPosition>(o => (TextAndPosition)o.Clone()));
+
+		this.QuickDelegate = that.QuickDelegate;
+		this.IndicateChoice = that.IndicateChoice;
+		this.selectTextNum = that.selectTextNum;
+		this.onlyShowTextNum = that.onlyShowTextNum;
+		this.streamTextNum = that.streamTextNum;
+		this.backgroundTextNum = that.backgroundTextNum;
+		this.Name = that.Name;
+		this.ChoiceType = that.ChoiceType;
+
+		testLog("in Choice Clone");
+		if(isEmptyList(that.MonsterList)){
+			testLog(that.MonsterList.Count);
+			//try{
+			if(that.MonsterList[0] != null){
+				testLog("in");
+				this.MonsterList = that.MonsterList.ConvertAll(new Converter<Monster, Monster>(o => (Monster)o.Clone()));
 			}
+			//}catch(Exception e){}
 		}
-		
-		List<TextAndPosition> onlyShowText;
-		public List<TextAndPosition> OnlyShowText{
-			get{
-				return onlyShowText;
-			}
-			set{
-				onlyShowText = value;
-				SetOSTNum();
-			}
-		}
-		
-		List<TextAndPosition> streamText;
-		public List<TextAndPosition> StreamText{
-			get{
-				return streamText;
-			}
-			set{
-				streamText = value;
-				Streaming();
-				SetSTNum();
-			}
-		}
-		
-		public void Streaming(){
-				foreach(TextAndPosition text in streamText){
-						text.isStream = true;
-				}
-			}
-		
-		List<TextAndPosition> backgroundText;
-		public List<TextAndPosition> BackgroundText{
-			get{
-				return backgroundText;
-			}
-			set{
-				backgroundText = value;
-				SetBGTNum();
-			}
-		}
-		
-		List<TextAndPosition> returnText;
-		public List<TextAndPosition> ReturnText{
-			get{
-				return returnText;
-			}
-			set{
-				returnText = value;
-			}
-		}
-		
-		Dictionary<int,String> indicateChoice;
-		public Dictionary<int,String> IndicateChoice{
-			get{
-				return indicateChoice;
-			}
-			set{
-				indicateChoice = value;
-			}
-		}
-		
-		List<Monster> monsterList;
-		public List<Monster> MonsterList{
-			get{
-				return monsterList;
-			}
-			set{
-				monsterList = value;
-			}
-		}
-		
-		List<NPC> npcList;
-		public List<NPC> NPCList{
-			get{
-				return npcList;
-			}
-			set{
-				npcList = value;
-			}
-		}
-		
-		ChoiceType choiceType;
-		public ChoiceType ChoiceType{
-			get;
-			set;
-		}
-		
-		public String ChoiceNext(int num){ //다음 선택지를 indicateChoice에서 찾아 반환
-			return indicateChoice[num];
-		}
-		
-		
-		void SetCTNum(){
-			CTNum = ChoiceText.Count;
-		}
-		void SetOSTNum(){
-			OSTNum = OnlyShowText.Count;
-		}
-		void SetSTNum(){
-			STNum = StreamText.Count;
-		}
-		void SetBGTNum(){
-			STNum = BackgroundText.Count;
-		}
-		
-		
-		public void Print(){
-			Console.WriteLine(choiceText[0].text+CTNum);
-		}
-		
-		public void QuickRun(){
-			QuickDelegate();
-		}
-		
-		public String QuickNext(){  //choiceType이 QUICKNEXT일때 빠르게 다음 선택지로 넘어갈때 DTG또는 Main에서 호출하는 함수
-			//Console.ReadKey();
-			return ChoiceNext(0);
-		}
-		
-		public List<TextAndPosition> CopyList(List<TextAndPosition> list){
-			List<TextAndPosition> copy = new List<TextAndPosition>();
-			for(int i = 0;i<list.Count;i++){
-				copy.Add((TextAndPosition)list[i].Clone());
-			}
-			return copy;
-		}
-		
-		public void LeaveChoice(){
-			if(ReturnText != null){
-				OnlyShowText = ReturnText;
-			}
-		}
-		
-		protected Choice(Choice that){
-			this.QuickDelegate = that.QuickDelegate;
-			
-			this.ChoiceText = new List<TextAndPosition>();
-			this.OnlyShowText = new List<TextAndPosition>();
-			this.StreamText = new List<TextAndPosition>();
-			this.BackgroundText = new List<TextAndPosition>();
-			this.MonsterList = new List<Monster>();
-			this.NPCList = new List<NPC>();
-			
-			
-			
-			if(that.ChoiceText != null)
-				this.ChoiceText = that.ChoiceText.ConvertAll(new Converter<TextAndPosition, TextAndPosition>(o => (TextAndPosition)o.Clone()));
-			if(that.OnlyShowText != null)
-				this.OnlyShowText = that.OnlyShowText.ConvertAll(new Converter<TextAndPosition, TextAndPosition>(o => (TextAndPosition)o.Clone()));
-			if(that.StreamText != null)
-				this.StreamText = that.StreamText.ConvertAll(new Converter<TextAndPosition, TextAndPosition>(o => (TextAndPosition)o.Clone()));
-			if(that.BackgroundText != null)
-				this.BackgroundText = that.BackgroundText.ConvertAll(new Converter<TextAndPosition, TextAndPosition>(o => (TextAndPosition)o.Clone()));
-			
-			this.IndicateChoice = that.IndicateChoice;
-			this.CTNum = that.CTNum;
-			this.OSTNum = that.OSTNum;
-			this.STNum = that.STNum;
-			this.BGTNum = that.BGTNum;
-			this.Name = that.Name;
-			this.ChoiceType = that.ChoiceType;
-			
-			testLog("in Choice Clone");
-			if(that.MonsterList != null){
-				isEmptyList(that.MonsterList);
-				try{
-				if(that.MonsterList[0] != null){
-					testLog("in");
-					this.MonsterList = that.MonsterList.ConvertAll(new Converter<Monster, Monster>(o => (Monster)o.Clone()));
-				}
-				}catch(Exception e){
-					
-				}
-			}
-			testLog("in Choice Clone2");
-			if(that.NPCList != null)
-				this.NPCList = that.NPCList.ConvertAll(new Converter<NPC, NPC>(o => (NPC)o.Clone()));
-		}
-		
-		public Object Clone(){
-			return new Choice(this);
-		}
+		testLog("in Choice Clone2");
+		if(that.NPCList != null)
+			this.NPCList = that.NPCList.ConvertAll(new Converter<NPC, NPC>(o => (NPC)o.Clone()));
+	}
+
+	public Object Clone(){
+		return new Choice(this);
+	}
 }
 	
 public class ChoiceControler{
-		public Dictionary<String,Choice> choiceDictionary; //선택지들을 담을 딕셔너리 클래스
-		
-		public ChoiceControler(){
-			choiceDictionary = new Dictionary<String,Choice>();
+	public Dictionary<String,Choice> choiceDictionary; //선택지들을 담을 딕셔너리 클래스
+
+	public ChoiceControler(){
+		choiceDictionary = new Dictionary<String,Choice>();
+	}
+	
+	public ChoiceControler(Scenario scenario):this(){
+		for(int i = 0;i<scenario.Count;i++){
+			choiceDictionary.Add(scenario[i].Name,(Choice)scenario[i].Clone());
 		}
-		
-		public Choice this[String index]{
-			get{
-				return choiceDictionary[index];
-			}
-			set{
-				choiceDictionary[index] = value;
-			}
+	}
+
+	public Choice this[String index]{
+		get{
+			return choiceDictionary[index];
 		}
-		
-		public Choice ChooseChoice(string choiceName){ //선택지를 골라낼 메소드
-			return choiceDictionary[choiceName];
+		set{
+			choiceDictionary[index] = value;
 		}
-		
-		public void AddChoice(Choice choice){ //선택지를 추가할 메소드
-			if(choiceDictionary.ContainsKey(choice.Name)){
-				choiceDictionary[choice.Name] = choice;
-			}else{
-				choiceDictionary.Add(choice.Name,choice);
-			}
+	}
+	
+	public void AddChoice(Choice choice){ //선택지를 추가할 메소드
+		if(choiceDictionary.ContainsKey(choice.Name)){
+			choiceDictionary[choice.Name] = choice;
+		}else{
+			choiceDictionary.Add(choice.Name,choice);
 		}
-		
-		public void AddScenario(Scenario scenario){ //선택지들의 뭉치인 시나리오를 통체로 추가할 메소드
-			for(int i = 0;i<scenario.Count;i++){
-				choiceDictionary.Add(scenario[i].Name,(Choice)scenario[i].Clone());
-			}
-		}
-		
-		public Choice SetChoice(String cName){
-			return choiceDictionary[cName];
-		}
-		
-		public Choice SetChoiceClone(String cName){
-			return (Choice)choiceDictionary[cName].Clone();
-		}
+	}
+
+	public Choice GetChoice(String choiceName){
+		return choiceDictionary[choiceName];
+	}
+
+	public Choice GetChoiceClone(String choiceName){
+		return (Choice)choiceDictionary[choiceName].Clone();
+	}
 		
 }
