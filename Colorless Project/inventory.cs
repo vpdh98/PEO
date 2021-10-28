4b4d9b2df282db2452ac9d76d2e295d843969430
vpdh98 <33365675+vpdh98@users.noreply.github.com>
2021-10-28 23:57:57 +0900

    Update inventory.cs

--- Colorless Project/inventory.cs
+++ Colorless Project/inventory.cs
@@ -6,11 +6,47 @@ using static GameWindows;
 
 public class Inventory{
 	List<Item> inventory;
+	Choice invenCho;
+	Dictionary<String,Item> invenListObject;
+	Backgrounds backgrounds = new Backgrounds();
+	DisplayTextGame IDTG = new DisplayTextGame();
 	
 	int maximumCount = 20;
 	
 	public Inventory(){
 		inventory = new List<Item>();
+	}
+	
+	void Init(){
+		
+		Dictionary<int,Object> invenListName = new Dictionary<int,Object>();
+		for(int i = 0;i<inventory.Count;i++){
+			invenListName.Add(i,inventory[i].Name);
+		}
+		List<TextAndPosition> itemList = new List<TextAndPosition>();
+		for(int i = 0;i<inventory.Count;i++){
+			if(i < 10)
+				itemList.Add(new TextAndPosition(inventory[i].Name,18,i+2,true));
+			else
+				itemList.Add(new TextAndPosition(inventory[i].Name,36,i-8,true));
+		}
+		invenListObject = new Dictionary<String,Item>();
+		for(int i = 0;i<inventory.Count;i++){
+			invenListObject.Add(inventory[i].Name,inventory[i]);
+		}
+	
+			invenCho = new Choice(){
+				Name = INVENTORY,
+				SelectText = itemList,
+				OnlyShowText = new List<TextAndPosition>()
+							{new TextAndPosition("인벤토리",28,1),
+							new TextAndPosition("버리기: D",52,1)},
+				IndicateChoice = invenListName,
+				BackgroundText = backgrounds.GetBackground(2)
+			};
+		IDTG.Init();
+		IDTG.Cho = invenCho; //초기 화면
+		IDTG.Show();
 	}
 	
 	public bool AddItem(Item item){
@@ -27,51 +63,25 @@ public class Inventory{
 	}
 	
 	public void OpenInventory(){
-			bool OutInven = false;
-		Backgrounds backgrounds = new Backgrounds();
-		
-		Dictionary<int,Object> invenListName = new Dictionary<int,Object>();
-		for(int i = 0;i<inventory.Count;i++){
-			invenListName.Add(i,inventory[i].Name);
-		}
-		List<TextAndPosition> itemList = new List<TextAndPosition>();
-		for(int i = 0;i<inventory.Count;i++){
-			if(i < 10)
-				itemList.Add(new TextAndPosition(inventory[i].Name,18,i+2,true));
-			else
-				itemList.Add(new TextAndPosition(inventory[i].Name,36,i-8,true));
-		}
-		Dictionary<String,Item> invenListObject = new Dictionary<String,Item>();
-		for(int i = 0;i<inventory.Count;i++){
-			invenListObject.Add(inventory[i].Name,inventory[i]);
-		}
-	
-			Choice invenCho = new Choice(){
-				Name = INVENTORY,
-				SelectText = itemList,
-				OnlyShowText = new List<TextAndPosition>()
-							{new TextAndPosition("인벤토리",28,1)},
-				IndicateChoice = invenListName,
-				BackgroundText = backgrounds.GetBackground(2)
-			};
-		
-		DisplayTextGame IDTG = new DisplayTextGame();
+		bool OutInven = false;
+		Init();
 		
 		while(!OutInven){
 				IDTG.Cho = invenCho; //초기 화면
-				//DTG.PrintListInfo();
 				IDTG.Show();
-				IDTG.delay = 0;
 				ConsoleKeyInfo c = Console.ReadKey();
 
-				while(c.Key != ConsoleKey.Escape){
+				while(c.Key != ConsoleKey.Escape && c.Key != ConsoleKey.I && inventory.Count != 0){
 
 					IDTG.SelectingText(c);
 
 					if(c.Key == ConsoleKey.Enter){
 						Item i = invenListObject[(String)IDTG.Cho.GetValueOn(IDTG.currentSelectNum)];
-						if(i.GetType().Name == "Weapon"){
-							if(ConfirmWindow("장비를 착용 하시겠습니까?",24,7)){
+						if(i is Equipment){
+							if(((Equipment)i).IsEquip){
+								i.Use();
+							}
+							else if(ConfirmWindow("장비를 착용 하시겠습니까?",24,7)){
 								i.Use();
 							}
 						}
@@ -79,6 +89,21 @@ public class Inventory{
 							if(ConfirmWindow("아이템을 사용하시겠습니까?",24,7)){
 								i.Use();
 							}
+						}
+					}
+					else if(c.Key == ConsoleKey.D){
+						Item i = invenListObject[(String)IDTG.Cho.GetValueOn(IDTG.currentSelectNum)];
+						if(i is Equipment){
+							if(((Equipment)i).IsEquip)
+								AlertWindow("장착된 장비는 버리실 수 없습니다.",24,9);
+							else if(ConfirmWindow("버리시겠습니까?",24,7)){
+								inventory.Remove(i);
+								Init();
+							}
+						}
+						else if(ConfirmWindow("버리시겠습니까?",24,7)){
+							inventory.Remove(i);
+							Init();
 						}
 					}
 					/*if(c.Key == ConsoleKey.Escape){
